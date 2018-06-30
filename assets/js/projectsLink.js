9401505744aea3b335ba301778d638cdb1d34f76
Lie Man <lieman@MacBook-Pro-de-Lie.local>
2018-06-30 19:53:09 +0200

    First commit

---
+++ assets/js/projectsLink.js
@@ -0,0 +1,26 @@
+$(document).ready(function(){
+    $(".axit").click(function(){
+        window.open('https://georgesmarie.github.io/AXIT/index.html', '_blank');
+    });
+
+    $(".cantus").click(function(){
+        window.open('https://georgesmarie.github.io/CANTUS/index.html', '_blank');
+    });
+
+    $(".furie").click(function(){
+        window.open('https://georgesmarie.github.io/Furie/index.html', '_blank');
+    });
+
+    $(".nava").click(function(){
+        window.open('https://georgesmarie.github.io/NAVA/index.html', '_blank');
+    });
+
+    $(".j-doe").click(function(){
+        window.open('https://georgesmarie.github.io/Musicians/index.html', '_blank');
+    });
+
+    $(".oud").click(function(){
+        window.open('https://georgesmarie.github.io/OUD/index.html', '_blank');
+    });
+});
+
